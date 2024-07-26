212783c584238586d3b6db1034674bead1988318
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-26 17:29:53 +0530

    Afficiation

--- DigitalClock.java
+++ DigitalClock.java
@@ -6,10 +6,14 @@ public class DigitalClock extends JFrame {
     private JLabel Clock, clockLabel;
 
     DigitalClock() {
+        Clockfunction();
         setLayout(null);
         setSize(400,400);
         setLocation(300,150);
         setTitle("My Clock");
         setVisible(true);
+    }
+    public void Clockfunction(){
+
     }
 }
